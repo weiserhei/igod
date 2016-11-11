d49b9f58ba893224e0cd8a29dbc1838902fdee49
weiserhei <falk@thiele-medien.de>
2016-11-11 21:14:17 +0100

    initial

---
+++ js/app/CharacterController.js
@@ -0,0 +1,53 @@
+/**
+ * Setup the control method
+define(["three","debugGUI"], 
+       function (THREE, debugGUI) {
+ */
+
+define(function(require) {
+
+        var	debugGUI = require('debugGUI');
+		
+		// return function () {};
+
+    'use strict';
+
+    var dg = debugGUI;
+    var guiFolder = dg.addFolder("Models");
+    guiFolder.open();
+
+    var activeCharacter = { update: function() {} }; // dummy function for update
+
+    function CharacterController(){
+    	this._activeCharacter = { update: function() {} };
+    }
+
+    CharacterController.prototype = {
+
+    	setActive: function( character ) {
+    		this._activeCharacter = character;
+    		activeCharacter = character;
+    	},
+   //  	disable: function( character ) {
+			// var index = activeCharacters.indexOf(character);
+			// if (index > -1) {
+			//     array.splice(index, 1);
+			// }
+   //  	},
+    	add: function( character ) {
+    		guiFolder.add( character, "load" ).name("Load "+character.getName());
+    		character.subscribeOnLoad( this.setActive );
+    	},
+    	/*
+    	load: function( character ) {
+    		character.load();
+    	},
+    	*/
+    	update: function( deltaTime ) {
+			activeCharacter.update( deltaTime );
+    	}
+
+    };
+
+    return CharacterController;
+});
---
+++ js/libs/three.js
@@ -0,0 +1,16 @@
+/**
+ * For creation of the global in requirejs
+ *
+ * Please add all addins here (and the main.js)
+ */
+define([
+		"threeCore",
+		"OrbitControls",
+		"OBJLoader",
+		// "MTLLoader",
+		"js/libs/three/loaders/collada/Animation.js",
+		"js/libs/three/loaders/collada/AnimationHandler.js",
+		"js/libs/three/loaders/collada/KeyFrameAnimation.js"
+       ], function (threeCore,OrbitControls,OBJLoader) {
+    return OrbitControls;
+});
